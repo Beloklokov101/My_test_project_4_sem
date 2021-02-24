3244e7c4eee3fc850e3f10bec96788ec998e732b
Beloklokov101 <58910434+Beloklokov101@users.noreply.github.com>
2021-02-24 18:39:42 +0300

    2-nd seminar classwork

--- My_test_project_4_sem/My_test_project_4_sem.cpp
+++ My_test_project_4_sem/My_test_project_4_sem.cpp
@@ -1,32 +1,50 @@
-﻿// My_test_project_4_sem.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
-#include <boost/align.hpp>
+//#include <boost/align.hpp>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello Git_2!\n";
-    //Index Editor
-    //Touch
+    std::cout << "N:";
+    int N;
+    std::cin >> N;
+
+    std::vector<int> a(N);
+    std::vector<int> b(N);
+    
+    std::cout << "a: ";
+    for (int i = 0; i < N; ++i) {
+        std::cin >> a[i];
+    }
+
+    std::cout << "b: ";
+    for (int i = 0; i < N; ++i) {
+        std::cin >> b[i];
+    }
+
+    auto its = b.begin();
+    auto itr = b.rbegin();
+    auto ita = a.begin();
+
+    for (int i = 0; i < N / 2; ++i) {
+        std::iter_swap(its, itr);
+        its++;
+        itr++;
+    }
+    its = b.begin();
+
+    for (int i = 0; i < N; ++i) {
+        std::iter_swap(ita, its);
+        ita++;
+        its++;
+    }
+
+    //std::reverse(b.begin(), b.end());
+    //a.swap(b);
+
+    std::cout << std::endl << "a  b" << std::endl;
+    for (int i = 0; i < N; ++i) {
+        std::cout << a[i] << " " << b[i] << std::endl;
+    }
 }
-
-//master brach commit
-
-//Not touch
-
-//Test branch commit
-//Second test branch commit
-
-//Git commit
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
